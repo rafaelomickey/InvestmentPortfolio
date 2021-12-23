e6fbd2863924d6eca494aae868e569fed6f52a4d
Rafael Ribeiro de Albuquerque <t804383@tokiomarine.com.br>
2021-12-23 13:10:05 -0300

    renomeando controller padrao

---
+++ src/InvestmentPortfolioApi/Controllers/FinanceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvestmentPortfolioApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FinanceController : ControllerBase
+    {
+        private readonly ILogger<FinanceController> _logger;
+
+        public FinanceController(ILogger<FinanceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Ola";
+        }
+    }
+}
